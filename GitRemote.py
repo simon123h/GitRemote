783a7a4818665d97546e0f34683365a6e6650ab3
simon123h <simonastronomie@gmail.com>
2016-11-26 23:07:57 +0100

    nothing basically

--- GitRemote.py
+++ GitRemote.py
@@ -6,3 +6,5 @@ ui.openMenu(menus.main)
 
 # TODO: make UNIX/Mac friendly
 # TODO: KOMMENTARE!!
+# TODO: idiotic menu: simplest commands, like a cloud: sync...
+#                                   (any other commands needed??)
