fe56f042f711068c5d2ce1021b47440931dfdab2
Simon Hartmann <simonastronomie@gmail.com>
2016-11-30 18:14:19 +0100

    elaborated the menus and functionality

--- functions.py
+++ functions.py
@@ -4,6 +4,11 @@ from ui import openMenu
 import menus
 
 
+"""
+    MENUS
+"""
+
+
 def main():
     openMenu(menus.main)
 
@@ -20,21 +25,85 @@ def branching():
     openMenu(menus.branching)
 
 
+def committing():
+    openMenu(menus.committing)
+
+
+def pulling():
+    openMenu(menus.pulling)
+
+
+"""
+    Pulling & Pushing
+"""
+
 
 def pull():
     git("pull")
 
 
-def commit(message=""):
-    git("add .")
-    if message == "":
-        git("commit")
-    else:
-        git("commit -m ''" + str(message) + "'")
+def pullDiscardLocal():
+    git("reset --hard")
+    git("pull")
+
+
+def pullKeepLocal():
+    git("add .")       # add local changes to staging area
+    git("stash")       # save local changes in stash & remove from working tree
+    git("pull")        # pull from remote
+    git("stash pop")   # reapply local changes
 
 
 def push():
     git("push")
+
+
+def init():
+    git("init")
+    url = input("Remote server url: ")
+    if url != "":
+        git("add .")
+        git("commit -m 'Initial commit'")
+        git("remote add origin " + url)
+        git("push --set-upstream origin master")
+
+
+"""
+    Committing
+"""
+
+
+def addAll():
+    git("add .")
+
+
+def commit(systemDialog=True, parameters=""):
+    git("add .")
+    if systemDialog:
+        git("commit"+parameters)
+    else:
+        message = input("Commit message: ")
+        git("commit -m ''" + message + "'"+parameters)
+
+
+def recommit():
+    commit(True, " --amend")
+
+
+def emptyCommit():
+    commit(False, "--allow-empty-message")
+
+
+def addAllCommitPush():
+    git("add .")
+    message = input("Commit message: ")
+    git("commit -m ''" + message + "'")
+    push()
+
+
+"""
+    Minor functions
+"""
 
 
 def status():
@@ -49,19 +118,74 @@ def log():
     git("log")
 
 
-def init():
-    git("init")
-    url = input("Remote server url: ")
-    if url != "":
-        addAll()
-        commit("Initial commit")
-        git("remote add origin " + url)
-        git("push --set-upstream origin master")
-
-
-def addAll():
-    git("add .")
-
-
 def quit():
     exit()
+
+
+"""
+    Branching
+"""
+
+
+def listBranches():
+    git("branch -av")
+
+
+def newBranch():
+    name = input("Create branch named: ")
+    git("checkout -b "+name)
+    git("push -u origin "+name)
+
+
+def switchBranch():
+    name = input("Switch to branch named: ")
+    git("checkout "+name)
+    git("push -u origin "+name)
+
+
+def deleteBranch():
+    name = input("Switch to branch named: ")
+    git("branch -d "+name)
+
+
+def merge():
+    name = input("Merge with branch named: ")
+    git("merge "+name)
+
+
+def rebase():
+    name = input("Switch to branch named: ")
+    git("rebase "+name)
+
+
+def setNewMaster():
+    name = input("Name of branch to set as new master: ")
+    git("checkout "+name)
+    git("merge --strategy=ours master")
+    git("checkout master")
+    git("merge "+name)
+
+
+def pushAllBranches():
+    git("push --all -u")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    pass
--- menus.py
+++ menus.py
@@ -12,6 +12,9 @@ main = [
             ['i', '', f.init],
             ['e', '', f.evenMore],
             ['a', '', f.addAll],
+            ['b', '', f.branching],
+            ['C', '', f.committing],
+            ['P', '', f.pulling],
             ['Q', '', f.quit]
         ], "Main Menu", 0
 
@@ -21,10 +24,13 @@ more = [
             ['s', 'Status', f.status],
             ['l', 'Log', f.log],
             ['b', 'Branching Menu', f.branching],
-            ['i', 'Init new repo', f.init],
             ['a', 'add All files', f.addAll],
+            ['i', 'Init new repo', f.init],
+            ['C', 'Committing Options', f.committing],
+            ['P', 'Pulling Options', f.pulling],
             ['e', 'even More', f.evenMore],
-            ['r', 'Return', f.main]
+            ['r', 'Return', f.main],
+            ['Q', 'Quit', f.quit]
         ], "More", 1
 
 
@@ -34,5 +40,28 @@ evenMore = [
 
 
 branching = [
-            ['r', 'Return', f.more]
-        ], "Branching", 2
+            ['n', 'New branch', f.newBranch],
+            ['l', 'List branches', f.listBranches],
+            ['s', 'Switch to branch', f.switchBranch],
+            ['d', 'Delete branch', f.deleteBranch],
+            ['m', 'Merge', f.merge],
+            ['b', 'Rebase', f.rebase],
+            ['p', 'Push all branches', f.pushAllBranches],
+            ['x', 'Set branch as new master', f.setNewMaster],
+            ['r', 'Return', f.main]
+        ], "Branching", 1
+
+
+committing = [
+            ['q', 'Add all, commit and push', f.addAllCommitPush],
+            ['a', 'Re-Commit', f.recommit],
+            ['e', 'Empty message commit', f.emptyCommit],
+            ['r', 'Return', f.main]
+        ], "Committing Options", 1
+
+
+pulling = [
+            ['d', 'Pull and discard local changes', f.pullDiscardLocal],
+            ['k', 'Pull and keep local changes', f.pullKeepLocal],
+            ['r', 'Return', f.main]
+        ], "Pulling Options", 1
