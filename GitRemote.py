d92914dcf1fb6af73ce675f749af96d0349d4a56
Simon Hartmann <simonastronomie@gmail.com>
2016-12-01 15:54:12 +0100

    'Testing finished. Everything works now basically. Jump back to here if anything breaks'

--- GitRemote.py
+++ GitRemote.py
@@ -3,13 +3,8 @@
 import ui
 import menus
 
-# local change4
-# remote change3
-
 ui.welcomeScreen()
 ui.openMenu(menus.main)
 
 
 # TODO: KOMMENTARE!!
-# TODO: idiotic menu: simplest commands, like a cloud: sync...
-#                                   (any other commands needed??)
